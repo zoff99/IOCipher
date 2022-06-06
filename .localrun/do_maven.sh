37ee16ab77fe7f8a2e19ae80d4d005ab35258f30
zoff99 <zoff99@users.noreply.github.com>
2022-06-06 10:45:39 +0200

    change to IOCipher2

--- .localrun/do_maven.sh
+++ .localrun/do_maven.sh
@@ -13,26 +13,26 @@ cd $_HOME_
 build_for='ubuntu_18.04'
 aar_dir=$_HOME_/"$build_for"/artefacts
 aar_file=$aar_dir/data-release.aar
-version="0.4.2.102"
+version="0.4.2.103"
 
 rm -Rf ./tmp/
 mkdir -p ./tmp/
 cp -av .m2 ./tmp/
 
-mv tmp/.m2/repository/info/guardianproject/iocipher/IOCipher/0.4.2 tmp/.m2/repository/info/guardianproject/iocipher/IOCipher/"$version"
-mv tmp/.m2/repository/info/guardianproject/iocipher/IOCipher/"$version"/IOCipher-0.4.2.pom \
-   tmp/.m2/repository/info/guardianproject/iocipher/IOCipher/"$version"/IOCipher-"$version".pom
-rm tmp/.m2/repository/info/guardianproject/iocipher/IOCipher/"$version"/IOCipher-0.4.2.aar
-cp -av "$aar_file" tmp/.m2/repository/info/guardianproject/iocipher/IOCipher/"$version"/IOCipher-"$version".aar
+mv tmp/.m2/repository/info/guardianproject/iocipher/IOCipher2/0.4.2 tmp/.m2/repository/info/guardianproject/iocipher/IOCipher2/"$version"
+mv tmp/.m2/repository/info/guardianproject/iocipher/IOCipher2/"$version"/IOCipher2-0.4.2.pom \
+   tmp/.m2/repository/info/guardianproject/iocipher/IOCipher2/"$version"/IOCipher2-"$version".pom
+rm tmp/.m2/repository/info/guardianproject/iocipher/IOCipher2/"$version"/IOCipher2-0.4.2.aar
+cp -av "$aar_file" tmp/.m2/repository/info/guardianproject/iocipher/IOCipher2/"$version"/IOCipher2-"$version".aar
 
-sed -i -e 's#0.4.2#'"$version"'#g' tmp/.m2/repository/info/guardianproject/iocipher/IOCipher/maven-metadata-local.xml
-sed -i -e 's#0.4.2#'"$version"'#g' tmp/.m2/repository/info/guardianproject/iocipher/IOCipher/"$version"/IOCipher-"$version".pom
+sed -i -e 's#0.4.2#'"$version"'#g' tmp/.m2/repository/info/guardianproject/iocipher/IOCipher2/maven-metadata-local.xml
+sed -i -e 's#0.4.2#'"$version"'#g' tmp/.m2/repository/info/guardianproject/iocipher/IOCipher2/"$version"/IOCipher2-"$version".pom
 
 cd tmp/
-zip -r local_maven_iocpiher_"$version".zip .m2
+zip -r local_maven_iocipher_"$version".zip .m2
 
 echo "##########################"
 echo ""
-ls -al $(pwd)/local_maven_iocpiher_"$version".zip
+ls -al $(pwd)/local_maven_iocipher_"$version".zip
 echo ""
 echo "##########################"
